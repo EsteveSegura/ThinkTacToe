b55a67b18705309274311c3f718dbd207fea6864
Esteve Segura <estevesegura.gir@gmail.com>
2025-05-25 22:18:44 +0200

    feat: new DTO trainer

--- src/train/train_dpo.py
+++ src/train/train_dpo.py
@@ -1,71 +1,25 @@
-import torch
-import multiprocessing
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from trl import DPOTrainer, DPOConfig
-from transformers import set_seed
+from trl import DPOConfig, DPOTrainer
+import json
 
-set_seed(42)
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
 
-# Modelo base
-model_name = "Qwen/Qwen2.5-1.5B"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer.pad_token = "<|endoftext|>"  # Ajusta según tu tokenizer
-tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
-tokenizer.padding_side = 'right'
+from datasets import Dataset
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    device_map="auto",
-    torch_dtype=torch.bfloat16
-)
+# Cargar datos desde el archivo JSON
+with open('./tictactoe_dpo.json', 'r') as f:
+    samples = json.load(f)
 
-# Dataset local
-dataset = load_dataset("json", data_files="tictactoe_dpo.json", split="train")
-dataset = dataset.train_test_split(test_size=0.01)
-train_dataset = dataset["train"]
-eval_dataset = dataset["test"]
+dataset = Dataset.from_list(samples)
 
-# Añadir <eos> si hace falta
-def process(example):
-    example["prompt"] = example["prompt"]
-    example["chosen"] = example["chosen"] + tokenizer.eos_token
-    example["rejected"] = example["rejected"] + tokenizer.eos_token
-    return example
-
-train_dataset = train_dataset.map(process, num_proc=multiprocessing.cpu_count(), load_from_cache_file=False)
-eval_dataset = eval_dataset.map(process, num_proc=multiprocessing.cpu_count(), load_from_cache_file=False)
-
-# Configuración del entrenamiento
-training_args = DPOConfig(
-    output_dir="./qwen2.5-1.5b-dpo",
-    per_device_train_batch_size=4,
-    per_device_eval_batch_size=4,
-    gradient_accumulation_steps=8,
-    eval_strategy="steps",
-    eval_steps=25,
-    logging_steps=10,
-    save_strategy="steps",
-    save_steps=200,
-    learning_rate=1e-6,
-    num_train_epochs=1,
-    max_length=1024,
-    max_prompt_length=1024,
-    warmup_ratio=0.1,
-    lr_scheduler_type="linear",
-    optim="paged_adamw_8bit",
-    bf16=True,
-    log_level="debug"
-)
-
-# Entrenador DPO
+training_args = DPOConfig(output_dir="Qwen2.5-1.5B-DPO")
 trainer = DPOTrainer(
     model=model,
     args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-    tokenizer=tokenizer
+    train_dataset=dataset,
+    processing_class=tokenizer
 )
 
-# Entrenamiento
 trainer.train()
