fdc047e758a23ca08fd820de18e0789fcc4a6cc4
Esteve Segura <estevesegura.gir@gmail.com>
2025-05-29 01:01:26 +0200

    feat: new tarin

--- src/train/train_optimize.py
+++ src/train/train_optimize.py
@@ -1,21 +1,18 @@
-# train_qwen_tictactoe.py
-
 import os
 import torch
 from huggingface_hub import login
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from datasets import load_dataset
-from trl import SFTTrainer
+from trl import SFTTrainer, SFTConfig
 
 # Login a Hugging Face
 login(token=os.environ["HF_TOKEN"])
 
 # Modelo base
-model_name = "Qwen/Qwen2.5-1.5B"
+model_name = "Qwen/Qwen2.5-0.5B"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
 
-# Asegurar que pad_token esté definido
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 model.resize_token_embeddings(len(tokenizer))
@@ -24,26 +21,30 @@ model.gradient_checkpointing_enable()
 
 dataset = load_dataset("json", data_files="tictactoe_hf.json", split="train")
 
-training_args = TrainingArguments(
-    output_dir="./qwen2.5-1.5b-tictactoe",
+def formatting_func(example):
+    return example["text"]
+
+training_args = SFTConfig(
+    output_dir="./qwen2.5-0.5b-tictactoe",
     per_device_train_batch_size=32,
-    num_train_epochs=3,
+    num_train_epochs=1,
     learning_rate=2e-5,
     logging_steps=10,
     save_steps=250,
     save_total_limit=1,
     report_to="none",
     fp16=True,
-    gradient_checkpointing=True
+    gradient_checkpointing=True,
+    max_seq_length=256,
+    eos_token="<|im_end|>",
 )
 
+# Trainer
 trainer = SFTTrainer(
     model=model,
     args=training_args,
     train_dataset=dataset,
-    tokenizer=tokenizer,
-    max_seq_length=256,
-    dataset_text_field="text"
+    formatting_func=formatting_func,
 )
 
 trainer.train()
