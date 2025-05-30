5986ee4529cf19e32b7b70cdfdb9560cac279d93
Esteve Segura <estevesegura.gir@gmail.com>
2025-05-30 20:05:18 +0200

    feat: improve dpo train params

--- src/train/train_dpo.py
+++ src/train/train_dpo.py
@@ -3,13 +3,12 @@ from trl import DPOConfig, DPOTrainer
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import json
 
-path = "./models/Qwen2.5-0.5B"
+path = "./qwen2.5-0.5b-tictactoe-sft/checkpoint-942"
 model = AutoModelForCausalLM.from_pretrained(path)
 tokenizer = AutoTokenizer.from_pretrained(path)
 
+train_dataset = load_dataset("json", data_files="./tictactoe_dpo.jsonl", split="train")
 
-train_dataset = load_dataset("json", data_files="tictactoe_dataset_sft.jsonl", split="train")
-
-training_args = DPOConfig(output_dir="Qwen2-0.5B-DPO-TicTacToe", logging_steps=10)
+training_args = DPOConfig(output_dir="qwen2.5-0.5b-tictactoe-dpo", logging_steps=10)
 trainer = DPOTrainer(model=model, args=training_args, processing_class=tokenizer, train_dataset=train_dataset)
 trainer.train()
